aaeb78bc8749aff2d91b755ca4dd70e883b68095
PVL-dev <99485287+PVL-dev@users.noreply.github.com>
2022-06-06 21:33:32 +0200

    Ajout de la validation du formulaire

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,4 @@
+// Ouverture/Fermeture du menu Responsive
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -7,24 +8,21 @@ function editNav() {
   }
 }
 
-// DOM Elements
+// Elements du DOM 
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalClose = document.querySelectorAll(".close");
-const formData = document.querySelectorAll(".formData");
 
-// launch modal event
+// Ouverture modale
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// launch modal form
 function launchModal() {
   modalbg.style.display = "flex";
 }
 
-// close modal event
+// Fermeture modale
 modalClose.forEach((btn) => btn.addEventListener("click", closeModal));
 
-// close modal form
 function closeModal() {
   modalbg.style.animationPlayState = "running";
   setTimeout(function() {
@@ -32,4 +30,70 @@ function closeModal() {
     modalbg.style.animationPlayState = "paused";
   }, 1000) 
 }
+
+
+// Validation du formulaire
+// Récupération des données du formulaire
+const formData = document.querySelectorAll(".formData");
+var email = document.querySelector("#email");
+var locationsContainer = document.querySelector("#locationsContainer");
+var locations = locationsContainer.querySelectorAll('[name="location"]');
+var terms = document.querySelector("#checkbox1");
+
+var locationError = document.getElementById("locationError");
+var termsError = document.getElementById("termsError");
+
+// Validation avancée de l'email
+var defaultRegexEmail = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]$/;
+var customRegexEmail = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/;
+
+var checkEmail = function () {
+  if (defaultRegexEmail.test(email.value) || email.value == "") {
+    var defaultValue = true;
+  } else {
+    var defaultValue = false;
+  }
+  
+  if (defaultValue == true && !customRegexEmail.test(email.value)) {
+    email.setCustomValidity("Il manque la dernière partie de l'adresse mail (.com, .fr...).");
+  } else {
+    email.setCustomValidity("");
+  }
+}
+
+email.addEventListener("input", checkEmail);
+
+// Validation avancée du choix du tournoi
+var enabledChoice = [];
+
+locations.forEach(function(radios) {
+  radios.addEventListener('change', function() {
+    enabledChoice = 
+      Array.from(locations).filter(i => i.checked).map(i => i.value);
+
+    console.log(enabledChoice);
+    locationError.innerHTML = "";
+  })
+});
+
+// Validation finale du formulaire
+function validate() {
+  console.log("Lancé")
+
+  if (enabledChoice == "") {
+    locationError.innerHTML = "<p>Vous devez choisir un tournoi.</p>";
+    return false;
+  }
+
+  if (!terms.checked) {
+    termsError.innerHTML = "<p>Vous devez accepter les conditions d'utilisation.</p>";
+    return false;
+  } else {
+    termsError.innerHTML = "";
+  }
+
+  return true;
+}
+
+// Gestion du comportement après validation
 
